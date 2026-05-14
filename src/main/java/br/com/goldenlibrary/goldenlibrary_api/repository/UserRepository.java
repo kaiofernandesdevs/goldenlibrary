1a55caa220495129eb41e2498c0878715559e670
kaio <kaioffernandesdias@gmail.com>
2026-05-13 21:19:08 -0300

    configurando security config e filtro do jwt

--- src/main/java/br/com/goldenlibrary/goldenlibrary_api/repository/UserRepository.java
+++ src/main/java/br/com/goldenlibrary/goldenlibrary_api/repository/UserRepository.java
@@ -3,5 +3,12 @@ package br.com.goldenlibrary.goldenlibrary_api.repository;
 import br.com.goldenlibrary.goldenlibrary_api.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
 }
