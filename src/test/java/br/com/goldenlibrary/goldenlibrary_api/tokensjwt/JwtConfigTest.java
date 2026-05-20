f1c6054c3cf2260d7a80764f63e88e36f739eb19
kaio <kaioffernandesdias@gmail.com>
2026-05-19 21:19:37 -0300

    corrigindo JwtServiceTest e implementando JwtAuthenticationFilterTest

--- src/test/java/br/com/goldenlibrary/goldenlibrary_api/tokensjwt/JwtConfigTest.java
+++ src/test/java/br/com/goldenlibrary/goldenlibrary_api/tokensjwt/JwtConfigTest.java
@@ -1,36 +1,46 @@
 package br.com.goldenlibrary.goldenlibrary_api.tokensjwt;
 
 import br.com.goldenlibrary.goldenlibrary_api.MongoIntegrationTest;
+import br.com.goldenlibrary.goldenlibrary_api.entity.User;
+import br.com.goldenlibrary.goldenlibrary_api.enums.UserRole;
+import br.com.goldenlibrary.goldenlibrary_api.security.CustomUserDetails;
+import br.com.goldenlibrary.goldenlibrary_api.service.JwtService;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static com.mongodb.internal.connection.tlschannel.util.Util.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest
 public class JwtConfigTest extends MongoIntegrationTest {
 
-    @Test
-    void shouldThrowExceptionWhenSecretIsNull() {
-        JwtConfig jwtConfig = new JwtConfig();
-
-        ReflectionTestUtils.setField(jwtConfig, "jwtSecretRaw", null);
-
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            jwtConfig.jwtSecretKey();
-        });
-
-        assertTrue(exception.getMessage().contains("A propriedade 'api.security.token.secret' nao foi configurada"));
-    }
-
+    @Autowired
+    private JwtService jwtService;
 
     @Test
-    void shouldThrowExceptionWhenSecretIsEmpty() {
-        JwtConfig jwtConfig = new JwtConfig();
+    void shouldGenerateAndExtractUserFromTokenSuccessfully() {
+        User user = new User();
+        user.setId("12345");
+        user.setName("Kaio Teste");
+        user.setEmail("test@goldenlibrary.com");
+        user.setRole(UserRole.USER);
 
-        ReflectionTestUtils.setField(jwtConfig, "jwtSecretRaw", "   ");
+        CustomUserDetails userDetails = new CustomUserDetails(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                null
+        );
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            jwtConfig.jwtSecretKey();
-        });
+        String token = jwtService.newToken(userDetails);
+        assertNotNull(token);
+
+        CustomUserDetails extractedUser = jwtService.getUserInToken(token);
+        assertNotNull(extractedUser);
+
+        assertEquals("test@goldenlibrary.com", extractedUser.getUsername());
+        assertEquals("Kaio Teste", extractedUser.getName());
+        assertEquals("12345", extractedUser.getId());
     }
 }
