04a7cf5f80f10d02ae73ad0403a362dd3154a225
kaio <kaioffernandesdias@gmail.com>
2026-06-04 16:18:47 -0300

    corrigindo a classe GoldenLibaryApiApplicationTests

--- src/test/java/br/com/goldenlibrary/goldenlibrary_api/GoldenlibraryApiApplicationTests.java
+++ src/test/java/br/com/goldenlibrary/goldenlibrary_api/GoldenlibraryApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.goldenlibrary.goldenlibrary_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GoldenlibraryApiApplicationTests extends MongoIntegrationTest {
 
 	@Test
